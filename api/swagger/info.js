eb8f848ad99b21ab53e188076cfdc25f14853de8
zacharyrote <zachary.rote@gmail.com>
2017-01-19 12:51:54 -0500

    added model-schema

---
+++ api/swagger/info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+   info: {
+     title: "Tenant Service",
+     description: "This service is responsible for segmenting service configuration between distinct client applications",
+     version: "1.0.0"
+   }
+ }
