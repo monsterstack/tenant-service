d39d59e0c76a4994feba4d04fb266ed3c0c4dae6
zacharyrote <zachary.rote@gmail.com>
2017-02-21 15:02:52 -0500

    tenant-service

--- api/swagger/model-schema.js
+++ api/swagger/model-schema.js
@@ -27,12 +27,9 @@
  *     type: object
  *     required:
  *        - cpuPercentUsage
- *        - totalMemPercentageUsage
  *        - loadAvg
  *     properties:
  *        cpuPercentUsage:
- *          type: number
- *        totalMemPercentageUsage:
  *          type: number
  *        loadAvg:
  *          type: number
