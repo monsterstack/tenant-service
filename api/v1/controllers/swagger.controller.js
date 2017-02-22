2a3f65dcdcc6f8cbefbbf63a00f6a50689be3cdc
zacharyrote <zachary.rote@gmail.com>
2017-02-21 20:46:12 -0500

    tenant-service

--- api/v1/controllers/swagger.controller.js
+++ api/v1/controllers/swagger.controller.js
@@ -10,7 +10,11 @@ const getSwagger = (app) => {
   return (req, res) => {
     let swaggerService = new SwaggerService('/api/v1');
     swaggerService.getSwagger().then((swagger) => {
-      res.status(HttpStatus.OK).send(swagger);
+      if(swagger === null) {
+        new ServiceError(HttpStatus.NOT_FOUND, 'Swagger Not Found').writeResponse(res);
+      } else {
+        res.status(HttpStatus.OK).send(swagger);
+      }
     }).catch((err) => {
       new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, err.message).writeResponse(res);
     });
